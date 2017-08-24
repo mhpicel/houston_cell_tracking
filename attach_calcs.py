20dc4042f03497f3726b1cd502b73319785a7b76
Mark Picel <mhpicel@gmail.com>
2017-08-24 12:11:09 -0500

    Added attach_calcs.py

---
+++ attach_calcs.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import dask.bag as db
+from find_storms import get_file_tree, parse_date_string
+from marcus_calcs import attach_marcus_stats, filename_from_dt
+
+
+def parse_storm_number(file):
+    storm_name = file.split('/')[-1]
+    storm_number = storm_name[-4:]
+    return storm_number
+
+
+def prep_storm_attach_stats(file, grid_dir):
+    tracks = pd.read_csv(file)
+    tracks.set_index(['scan', 'uid'], inplace=True)
+    tracks['time'] = tracks['time'].apply(parse_date_string)
+    tracks['file'] = tracks['time'].apply(lambda dt:
+                                          filename_from_dt(dt, grid_dir))
+    return parse_storm_number(file), attach_marcus_stats(tracks)
+
+
+if __name__ == '__main__':
+    track_dir = ''
+    grid_dir = '/lcrc/group/earthscience/radar/houston/data/'
+    out_dir = ''
+    pattern = "storm_*"
+    files = get_file_tree(track_dir, pattern)
+    files.sort()
+    track_bag = db.from_sequence(files, npartitions=36)
+    track_bag = track_bag.map(lambda file:
+                              prep_storm_attach_stats(file, grid_dir))
+    out_tracks = track_bag.compute()
+    for number, track in out_tracks:
+        track.tracks.to_csv(out_dir + 'stormcalcs_' + number + '.csv')
